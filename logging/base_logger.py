5967447b2894ba192255270bbe690e0357213409
francescomandarino15 <francescomandarino81@gmail.com>
2025-11-19 15:45:03 +0100

    Create base_logger.py

---
+++ logging/base_logger.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class BaseLogger(ABC):
+    def __init__(self, log_per_batch: bool, log_per_epoch: bool):
+        self.log_per_batch = log_per_batch
+        self.log_per_epoch = log_per_epoch
+
+    @abstractmethod
+    def log_batch(self, step: int, flops: int, cumulative_flops: int, epoch: int | None = None):
+        ...
+
+    @abstractmethod
+    def log_epoch(self, epoch: int, flops: int, cumulative_flops: int):
+        ...
+
+    @abstractmethod
+    def close(self):
+        ...
